16c221e4dfbaa4a3780a027f3a10cce543dceaf7
Aman8815 <amanyashona465@gmail.com>
2025-08-29 13:48:47 +0530

    done

---
+++ Hibernate-practicals/src/com/rays/user/EmployeeModel.java
@@ -0,0 +1,79 @@
+package com.rays.user;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+
+public class EmployeeModel {
+	
+	
+	 public void add(EmployeeDTO dto) {
+		 
+		 SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+			Session session = sf.openSession();
+
+			Transaction tx = session.beginTransaction();
+
+			session.save(dto);
+
+			tx.commit();
+
+			session.close();
+		 
+	 }
+	 
+	 public void update(EmployeeDTO dto) {
+		 
+		 SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		 
+		 Session session = sf.openSession();
+		 Transaction tx = session.beginTransaction();
+		 
+		 session.update(dto);
+		 
+		 tx.commit();
+		 session.close();
+		 
+	 }
+	 
+	 public List search(EmployeeDTO dto, int pageSize, int pageNo) {
+		 
+		 List list = null;
+		 SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		 Session session = sf.openSession();
+	
+		 Criteria criteria = session.createCriteria(EmployeeDTO.class);
+
+			if (dto != null) {
+
+				if (dto.getName() != null && dto.getName().length() > 0) {
+
+					criteria.add(Restrictions.like("firstName", dto.getName() + "%"));
+
+				}
+			}
+
+			if (pageSize > 0) {
+				pageNo = (pageNo - 1) * pageSize;
+				criteria.setFirstResult(pageNo);
+				criteria.setMaxResults(pageSize);
+			}
+
+			 list = criteria.list();
+
+			session.close();
+
+			return list;
+		 
+		  
+		
+		 
+	 }
+
+}
---
+++ Hibernate-practicals/src/com/rays/user/TestEmployee.java
@@ -0,0 +1,59 @@
+package com.rays.user;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class TestEmployee {
+	
+	
+	public static void main(String[] args) {
+	     //add();
+		//update();
+		search();
+	}
+
+	private static void search() {
+		EmployeeDTO dto = new EmployeeDTO();
+		EmployeeModel model = new EmployeeModel();
+		
+		List list = model.search(dto, 0, 0);
+		
+		Iterator it = list.iterator();
+		
+		while(it.hasNext()) {
+			dto = (EmployeeDTO) it.next();
+			System.out.print(dto.getId());
+			System.out.print("\t"+dto.getName());
+			System.out.print("\t"+dto.getAddress());
+			System.out.print("\t"+dto.getSalary());
+			System.out.println();
+		}
+		
+	}
+
+	private static void update() {
+		EmployeeDTO dto = new EmployeeDTO();
+		EmployeeModel model = new EmployeeModel();
+		 
+		dto.setId(1);
+		dto.setName("Aman");
+		dto.setAddress("Indore");
+		dto.setSalary(13000);
+		model.update(dto);
+		
+	}
+
+	private static void add() {
+		
+		EmployeeDTO dto = new EmployeeDTO();
+		EmployeeModel model = new EmployeeModel();
+		
+		dto.setId(1);
+		dto.setName("Aman");
+		dto.setAddress("Indore");
+		dto.setSalary(12000);
+		  model.add(dto);
+	
+	}
+
+}
